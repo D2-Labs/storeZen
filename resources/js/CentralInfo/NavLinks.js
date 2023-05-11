03c7efb2c71aec17b4d5f4d2f83969ac9f049ef8
Dacosta <dacsolo10@gmail.com>
2023-05-11 23:57:58 +0000

    added nav links to keepers page

--- resources/js/CentralInfo/NavLinks.js
+++ resources/js/CentralInfo/NavLinks.js
@@ -1,36 +1,59 @@
-import { IoMdNotificationsOutline, TfiLayoutGrid2, AiOutlineShoppingCart, IoPricetagsOutline, IoPeopleOutline} from 'react-icons/all';
+import {
+    IoMdNotificationsOutline,
+    TfiLayoutGrid2,
+    AiOutlineShoppingCart,
+    IoPricetagsOutline,
+    IoPeopleOutline,
+    HiOutlineSquare3Stack3D,
+    MdFormatIndentDecrease,
+    MdFormatIndentIncrease,
+} from "react-icons/all";
 
 export const salesLinks = [
     {
-        'name': 'Dashboard',
-        'icon': TfiLayoutGrid2,
-        'link': '/sales',
+        name: "Dashboard",
+        icon: TfiLayoutGrid2,
+        link: "/sales",
     },
 
     {
-        'name': 'Sales',
-        'icon': AiOutlineShoppingCart,
-        'link': '/sales/sales-made',
+        name: "Sales",
+        icon: AiOutlineShoppingCart,
+        link: "/sales/sales-made",
     },
 
     {
-        'name': 'Statistics',
-        'icon': IoPricetagsOutline,
-        'link': '/sales/sales-stats',
+        name: "Statistics",
+        icon: IoPricetagsOutline,
+        link: "/sales/sales-stats",
     },
 
     {
-        'name': 'Users',
-        'icon': IoPeopleOutline,
-        'link': '/sales/profile',
-    }
+        name: "Users",
+        icon: IoPeopleOutline,
+        link: "/sales/profile",
+    },
 ];
 
 export const keeperLinks = [
     {
-        'name': 'Users',
-        'icon': IoPeopleOutline,
-        'link': '/keeper/sales-persons',
-    }
-]
-
+        name: "Users",
+        icon: IoPeopleOutline,
+        link: "/keeper/sales-persons",
+    },
+    {
+        name: "Products",
+        icon: HiOutlineSquare3Stack3D,
+        link: "/keeper/product-listings",
+    },
+    {
+        name: "Orders",
+        icon: MdFormatIndentDecrease,
+        link: "/keeper/goods-order-form",
+    },
+    {
+        name: "Goods coming in",
+        icon: MdFormatIndentIncrease,
+        link: "/keeper/goods-receiving-form",
+    },
+];
